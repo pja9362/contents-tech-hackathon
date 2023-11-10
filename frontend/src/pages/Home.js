c7e0c18e8c057b5717e0f77d327dd45d7b38a1cd
jina <pja9362@g.skku.edu>
2023-11-10 20:15:39 +0900

    feat(auth, enroll): 디자인 완료

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -5,8 +5,13 @@ import {
   StyleSheet,
   TouchableOpacity,
   TextInput,
+  Image,
 } from "react-native";
 import AsyncStorage from "@react-native-async-storage/async-storage";
+import topLogo from "../images/top_logo.png";
+import mainPlant from "../images/mainPlant.png";
+import { AntDesign } from "@expo/vector-icons";
+import StepIndicator from "../components/StepIndicator";
 
 const Home = ({ navigation }) => {
   const API_URL = "http://192.168.0.29:3000";
@@ -18,7 +23,7 @@ const Home = ({ navigation }) => {
 
   const getData = async () => {
     try {
-      const value = await AsyncStorage.getItem('username');
+      const value = await AsyncStorage.getItem("username");
       if (value !== null && value !== undefined) {
         setUsername(value);
         console.log("유저 정보:", value);
@@ -53,6 +58,7 @@ const Home = ({ navigation }) => {
 
         if (response.status === 200) {
           alert("식물 등록이 완료되었습니다.");
+          AsyncStorage.setItem("plantName", plantName);
           navigation.navigate("Welcome");
         } else {
           alert("올바른 정보를 입력해주세요");
@@ -65,46 +71,46 @@ const Home = ({ navigation }) => {
 
   return (
     <View style={styles.container}>
-      <View
-        style={{
-          marginTop: "40%",
-          width: 150,
-          height: 150,
-          borderRadius: 75,
-          backgroundColor: "lightgray",
-        }}
-      ></View>
-
-      {currentStep === 1 && (
-        <View>
-          <Text style={styles.mainText}>나의 식물을 골라주세요~!</Text>
-          <View >
-            <TextInput
-              placeholder="나의 식물 종류를 골라주세요~!"
-              onChangeText={(text) => setPlantType(text)}
-              value={plantType}
-            />
-          </View>
+      <Image source={topLogo} style={styles.topLogo} />
+      <Text style={styles.welcomeText}>Hello Plant!</Text>
+      <View style={styles.imageContainer}>
+        <Image source={mainPlant} style={styles.mainPlant} />
+      </View>
+      <View style={styles.bottomSheet}>
+        {currentStep === 1 && (
+          <>
+            <Text style={styles.mainText}>반려 식물을 선택해주세요</Text>
+            <View style={styles.inputContainer}>
+              <TextInput
+                style={styles.input}
+                placeholder="나의 식물 종류를 골라주세요~!"
+                onChangeText={(text) => setPlantType(text)}
+                value={plantType}
+              />
+            </View>
+          </>
+        )}
+        {currentStep === 2 && (
+          <>
+            <Text style={styles.mainText}>반려 식물의 이름이 무엇인가요?</Text>
+            <View style={styles.inputContainer}>
+              <TextInput
+                style={styles.input}
+                placeholder="내 반려 식물의 이름은?!"
+                onChangeText={(text) => setPlantName(text)}
+                value={plantName}
+              />
+            </View>
+          </>
+        )}
+      <View style={{marginTop: '50%'}}>
+        <StepIndicator steps={['1', '2']} currentStep={currentStep - 1} />
         </View>
-      )}
-      {currentStep === 2 && (
-        <View>
-          <Text style={styles.mainText}>내 반려식물의 이름은?!</Text>
-          <View>
-            <TextInput
-              placeholder="내 반려식물의 이름은?!"
-              onChangeText={(text) => setPlantName(text)}
-              value={plantName}
-            />
-          </View>
-        </View>
-      )}
-
-      <Text style={styles.indexText}>{currentStep}/2</Text>
-
-      <TouchableOpacity style={styles.button} onPress={onNextStep}>
-        <Text>{currentStep === 2 ? "완료" : "다음"}</Text>
-      </TouchableOpacity>
+        <TouchableOpacity style={styles.button} onPress={onNextStep}>
+          <Text style={styles.buttonText}>NEXT</Text>
+          <AntDesign name="right" size={20} color="white" />
+        </TouchableOpacity>
+      </View>
     </View>
   );
 };
@@ -113,25 +119,86 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#fff",
+  },
+  topLogo: {
+    width: 160,
+    height: 40,
+    resizeMode: "contain",
+    marginTop: 40,
+  },
+  welcomeText: {
+    fontSize: 18,
+    fontWeight: "bold",
+    marginLeft: 65,
+    marginTop: 10,
+    color: '#6D371E'
+  },
+  imageContainer: {
+    width: "60%",
+    marginHorizontal: "20%",
+    marginTop: 60,
+    marginBottom: -15,
+    zIndex: 2,
+    aspectRatio: 1,
+    borderRadius: 125,
+    backgroundColor: "#fff",
+    justifyContent: "center",
     alignItems: "center",
-    padding: 20,
+    borderWidth: 2,
+    borderColor: "#CCDF9D",
+  },
+  mainPlant: {
+    width: 320,
+    height: 250,
+    resizeMode: "contain",
+    alignSelf: "center",
+    marginBottom: 25,
   },
   mainText: {
     fontSize: 18,
     fontWeight: "bold",
-    marginVertical: 50,
+    marginVertical: 25,
+    textAlign: "center",
   },
-  indexText: {
-    fontSize: 12,
-    marginTop: 70,
-    marginBottom: 30,
+  inputContainer: {
+    width: "100%",
+    alignItems: "center",
+  },
+  input: {
+    width: "80%",
+    height: 40,
+    backgroundColor: "#fff",
+    borderRadius: 15,
+    borderWidth: 1,
+    borderColor: "#D6EAD6",
+    paddingHorizontal: 20,
+  },
+  bottomSheet: {
+    backgroundColor: "#F4F8E9",
+    width: "100%",
+    height: "60%",
+    borderTopLeftRadius: 30,
+    borderTopRightRadius: 30,
+    alignItems: "center",
   },
   button: {
-    width: "80%",
-    height: 50,
-    backgroundColor: "lightgray",
+    width: "50%",
+    height: 40,
+    backgroundColor: "#CCDF9D",
     justifyContent: "center",
     alignItems: "center",
+    zIndex: 3,
+    bottom: 100,
+    position: "absolute",
+    borderRadius: 20,
+    flexDirection: "row",
+    gap: 5,
+  },
+  buttonText: {
+    fontSize: 16,
+    fontWeight: "bold",
+    color: "#6D371E",
+    paddingLeft: 10,
   },
 });
 
--- frontend/src/pages/Welcome.js
+++ frontend/src/pages/Welcome.js
@@ -1,30 +1,56 @@
-import React from "react";
-import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
+import React, { useState, useEffect } from "react";
+import {
+  View,
+  Text,
+  StyleSheet,
+  TouchableOpacity,
+  TextInput,
+  Image,
+} from "react-native";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import topLogo from "../images/top_logo.png";
+import mainPlant from "../images/mainPlant.png";
+import { AntDesign } from "@expo/vector-icons";
 
 const Welcome = ({ navigation }) => {
+  const [plantName, setPlantName] = useState("");
 
-    const onNextStep = () => {
-        navigation.navigate('Main');
+  const getData = async () => {
+    try {
+      const value = await AsyncStorage.getItem("plantName");
+      if (value !== null && value !== undefined) {
+        setPlantName(value);
+      }
+    } catch (error) {
+      console.error("로컬 스토리지 에러:", error);
     }
+  };
+
+  useEffect(() => {
+    getData();
+  }, []);
+  const onNextStep = () => {
+    navigation.navigate("Main");
+  };
 
   return (
     <View style={styles.container}>
-      {/* image */}
-      <View
-        style={{
-          marginTop: "40%",
-          width: 150,
-          height: 150,
-          borderRadius: 75,
-          backgroundColor: "lightgray",
-        }}
-      ></View>
+      <Image source={topLogo} style={styles.topLogo} />
+      <Text style={styles.welcomeText}>Hello {plantName}!</Text>
+      <View style={styles.imageContainer}>
+        <Image source={mainPlant} style={styles.mainPlant} />
+      </View>
 
-      <Text style={styles.mainText}>입장을 환영합니다~</Text>
+      <View style={styles.bottomSheet}>
+        <Text style={styles.mainText}>
+          입장을 환영합니다.{"\n"}마음놓고 보호하는 서비스
+        </Text>
 
-      <TouchableOpacity style={styles.button} onPress={onNextStep}>
-        <Text>입장</Text>
-      </TouchableOpacity>
+        <TouchableOpacity style={styles.button} onPress={onNextStep}>
+          <Text style={styles.buttonText}>ENTER</Text>
+          <AntDesign name="right" size={20} color="white" />
+        </TouchableOpacity>
+      </View>
     </View>
   );
 };
@@ -33,18 +59,74 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#fff",
+  },
+  topLogo: {
+    width: 160,
+    height: 40,
+    resizeMode: "contain",
+    marginTop: 40,
+  },
+  welcomeText: {
+    fontSize: 18,
+    fontWeight: "bold",
+    marginLeft: 65,
+    marginTop: 10,
+    color: "#6D371E",
+  },
+  imageContainer: {
+    width: "60%",
+    marginHorizontal: "20%",
+    marginTop: 60,
+    marginBottom: -15,
+    zIndex: 2,
+    aspectRatio: 1,
+    borderRadius: 125,
+    backgroundColor: "#fff",
+    justifyContent: "center",
     alignItems: "center",
-    padding: 20,
+    borderWidth: 2,
+    borderColor: "#CCDF9D",
+  },
+  mainPlant: {
+    width: 320,
+    height: 250,
+    resizeMode: "contain",
+    alignSelf: "center",
+    marginBottom: 25,
   },
   mainText: {
     fontSize: 18,
     fontWeight: "bold",
     marginVertical: 50,
+    textAlign: "center",
+    lineHeight: 25,
+  },
+  bottomSheet: {
+    backgroundColor: "#F4F8E9",
+    width: "100%",
+    height: "60%",
+    borderTopLeftRadius: 30,
+    borderTopRightRadius: 30,
+    alignItems: "center",
   },
   button: {
-    height: 50,
+    width: "50%",
+    height: 40,
+    backgroundColor: "#CCDF9D",
     justifyContent: "center",
     alignItems: "center",
+    zIndex: 3,
+    bottom: 100,
+    position: "absolute",
+    borderRadius: 20,
+    flexDirection: "row",
+    gap: 5,
+  },
+  buttonText: {
+    fontSize: 16,
+    fontWeight: "bold",
+    color: "#6D371E",
+    paddingLeft: 10,
   },
 });
 
